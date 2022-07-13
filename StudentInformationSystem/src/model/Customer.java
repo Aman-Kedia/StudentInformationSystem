aae65b4df7ae7b5acefdfbfe09d275cfe49da7ad
amank <amank@DESKTOP-LMI577H>
2022-07-13 15:38:11 +0530

    updated

--- StudentInformationSystem/src/model/Customer.java
+++ StudentInformationSystem/src/model/Customer.java
@@ -4,6 +4,7 @@ public class Customer {
 	int customerId;
 	String customerName;
 	int customerAge;
+	String customerDOB;
 	
 	public int getCustomerId() {
 		return customerId;
@@ -27,6 +28,14 @@ public class Customer {
 
 	public void setCustomerAge(int customerAge) {
 		this.customerAge = customerAge;
+	}
+
+	public String getCustomerDOB() {
+		return customerDOB;
+	}
+
+	public void setCustomerDOB(String customerDOB) {
+		this.customerDOB = customerDOB;
 	}
 	
 }
